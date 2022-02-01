f97cca2a31d82b07d926e3b15f772a5cdf1b1266
Evan Zona <evanbzona@gmail.com>
2022-02-01 10:33:06 -0600

    Initial Commit

---
+++ API hackwitch/API_hackwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_hackwitchApp.swift
+//  API hackwitch
+//
+//  Created by Student on 2/1/22.
+//
+
+import SwiftUI
+
+@main
+struct API_hackwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
